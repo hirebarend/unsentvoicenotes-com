569afa656b973d6b4bfb153f177858ca54848411
Barend Erasmus <hirebarend@gmail.com>
2023-03-31 14:47:17 +0200

    update

--- src/routes/home-route.tsx
+++ src/routes/home-route.tsx
@@ -110,6 +110,28 @@ export function HomeRoute() {
             </Button>
           </div>
           {result.data && result.data.length ? (
+            <ListGroup as="ol" variant="flush">
+              {result.data.map((x) => (
+                <ListGroup.Item as="li" key={x.timestamp}>
+                  <div>
+                    <div className="fs-6 fw-semibold text-center">
+                      {moment(x.timestamp).format("dddd")} at{" "}
+                      {moment(x.timestamp).format("HH:mm")}
+                    </div>
+                    <div className="py-1">
+                      <audio controls src={x.url} />
+                    </div>
+                  </div>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          ) : (
+            <div className="p-5">
+              <img className="w-100" src="/undraw_notes_re_pxhw.svg" />
+            </div>
+          )}
+
+          {/* {result.data && result.data.length ? (
             <ListGroup as="ol">
               {result.data.map((x) => (
                 <ListGroup.Item as="li" key={x.timestamp}>
@@ -129,7 +151,7 @@ export function HomeRoute() {
             <div className="p-5">
               <img className="w-100" src="/undraw_notes_re_pxhw.svg" />
             </div>
-          )}
+          )} */}
         </Card.Body>
       </Card>
     </>
